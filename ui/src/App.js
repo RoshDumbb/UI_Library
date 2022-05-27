6c123c03ae6c5dcfff222e4748367f507b9b436e
RoshDumbb <roshanidumbhare30@gmail.com>
2022-05-27 17:50:09 +0530

    Login page of amazon

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './ui/Login';
+import Amazon from './ui/Amazon';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <Login/>
+        {/* <Login/> */}
+        <Amazon/>
       </header>
     </div>
   );
